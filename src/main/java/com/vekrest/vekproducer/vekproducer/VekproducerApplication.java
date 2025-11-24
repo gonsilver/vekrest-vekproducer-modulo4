abfb66ec91f6dcee362b1b4ba645249d42c118f3
vek03 <vektromboni@gmail.com>
2025-11-24 19:51:13 -0300

    :tada: First Commit

---
+++ src/main/java/com/vekrest/vekproducer/vekproducer/VekproducerApplication.java
@@ -0,0 +1,13 @@
+package com.vekrest.vekproducer.vekproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VekproducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VekproducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vekrest/vekproducer/vekproducer/VekproducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vekrest.vekproducer.vekproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VekproducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
